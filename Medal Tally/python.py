45bdba827c6effc02061153d27586c6cad900cde
snoozingnewt <78986317+snoozingnewt@users.noreply.github.com>
2026-02-20 11:09:36 -0500

    Update python.py

--- Medal Tally/python.py
+++ Medal Tally/python.py
@@ -1,2 +1,2 @@
 import sys
-for s in sys.argv[1:]:l=s.split();a,b,c=map([*map(l.index,l)].count,range(3));print('1💎 '*(a<2)+f'{a}🥇'+f' {b}🥈'*(b>0)+f' {c}🥉'*(c>0))
+for s in sys.argv[1:]:l=s.split();a,b,c=map([*map(l.index,l)].count,(0,1,2));print('1💎 '*(a<2)+f'{a}🥇'+f' {b}🥈'*(b>0)+f' {c}🥉'*(c>0))
